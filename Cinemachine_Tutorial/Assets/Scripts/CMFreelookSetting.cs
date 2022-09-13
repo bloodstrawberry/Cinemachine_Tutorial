bfeaae5f29ce04646cbbf722d6dc8e47168097c4
bloodstrawberry <vvv3334@hanmail.net>
2022-09-13 17:07:12 +0900

    control freelook camera by script
    스크립트로 freelook 카메라 제어하기

--- Cinemachine_Tutorial/Assets/Scripts/CMFreelookSetting.cs
+++ Cinemachine_Tutorial/Assets/Scripts/CMFreelookSetting.cs
@@ -5,9 +5,35 @@ using Cinemachine;
 
 public class CMFreelookSetting : MonoBehaviour
 {
+    CinemachineFreeLook freeLook;
     void Awake()
     {
         CinemachineCore.GetInputAxis = clickControl;
+    }
+
+    void Start()
+    {
+        // Target 설정
+        freeLook = this.GetComponent<CinemachineFreeLook>();
+        freeLook.Follow = GameObject.Find("PlayerArmature").transform;
+        freeLook.LookAt = GameObject.Find("PlayerCameraRoot").transform;
+
+        // Lens 설정
+        freeLook.m_Lens.FieldOfView = 50;
+        freeLook.m_Lens.NearClipPlane = 0.2f;
+        freeLook.m_Lens.FarClipPlane = 4000;
+        freeLook.m_Lens.Dutch = 0;
+        freeLook.m_Lens.ModeOverride = LensSettings.OverrideModes.None;
+
+        // Axis Control 설정
+        freeLook.m_XAxis.Value = 0;
+        freeLook.m_XAxis.m_MaxSpeed = 300;
+        freeLook.m_XAxis.m_AccelTime = 0.1f;
+
+        // Rig 설정
+        CinemachineComposer composer = freeLook.GetRig(1).GetCinemachineComponent<CinemachineComposer>();
+        composer.m_DeadZoneHeight = 0.3f;
+        composer.m_DeadZoneWidth = 0.5f;
     }
 
     public float clickControl(string axis)
